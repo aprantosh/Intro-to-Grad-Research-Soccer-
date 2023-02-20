43900c3c2ec65da5f2a7d7459063b9d91a08c314
aprantosh <aprantosh@gmail.com>
2023-02-20 13:47:11 -0600

    data been scaled

--- initial.R
+++ initial.R
@@ -1,1 +1,23 @@
-df <- read.csv("C://Users/prantosh/Desktop/Final_data_research.csv")
+setwd("C://Users/prantosh/Documents/Intro_to_Grad_Research/Intro_to_Graduate_Research")
+library("readxl")
+library(tidyverse)
+
+library("readxl")
+
+# Read xlsx files
+soccer_collection = read_excel("C://Users/prantosh/Documents/Intro_to_Grad_Research/Intro_to_Graduate_Research/Final_data_research.xlsx")
+
+View(soccer_collection)
+test <- soccer_collection
+#scale(select(soccer_collection, -Gender, -Squad))
+a <- scale(select(test, -Gender,-Squad))
+Set_after_scaling <- data.frame(test$Gender, test$Squad,a)
+                
+View(Set_after_scaling)
+
+install.packages("NbClust")
+library("NbClust")
+
+NbClust(a)
+
+
